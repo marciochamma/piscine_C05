21fbcf97ff2e5553b7b97cb2981a41eaca640fa2
marciochamma <82783953+marciochamma@users.noreply.github.com>
2024-03-11 00:20:16 -0300

    Update ft_iterative_power.c

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include<unistd.h>
-// #include<stdio.h>
+#include<stdio.h>
 
 int	ft_iterative_power(int nb, int power)
 {
